4b22f3a8d6073814507609a64e909088b1865ed0
NageebSalma <sleem.eaas@gmail.com>
2022-02-14 13:29:20 +0200

    remove nested repos

---
+++ django-chat-app/chat/pusher.py
@@ -0,0 +1,10 @@
+import pusher
+
+pusher_client = pusher.Pusher(
+  app_id='1347141',
+  key='50a34d118d0cc1910964',
+  secret='940e3dcd42da3d10d724',
+  cluster='eu',
+  ssl=True
+)
+
---
+++ django-chat-app/chat/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from chat.views import MessageAPIView
+urlpatterns = [
+    path('messages' , MessageAPIView.as_view())
+]
+
